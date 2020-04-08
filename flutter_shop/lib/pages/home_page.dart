3a471564519b3e65cea8dc46e6ac81b693c251f1
yhbj <yanchaobo@yhbj.cn>
2020-04-09 00:09:25 +0800

    首页 基本 完成

--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -18,18 +18,25 @@ class _HomePageState extends State<HomePage> {
     return Container(
       child: Scaffold(
           appBar: AppBar(
-            title: Text('美好人间'),
+            title: Text('私人情趣+'),
           ),
           body: FutureBuilder(
             builder: (context, snapshot) {
               if (snapshot.hasData) {
                 List list = (snapshot.data["users"]);
                 List navList = posts.sublist(0,10);
+                String adPicture = "http://img.pconline.com.cn/images/upload/upc/tx/photoblog/1408/26/c0/37863154_37863154_1409006331562.jpg";
 //                litpic
                 return Column(
                   children: <Widget>[
+//                    1
                     SwiperDiy(swiperDateList: list),
+//                    2
                     TopNavigator(navigatorList: navList),
+//                    3
+                    AdBanner(adPicture: adPicture),
+//                    4
+                  AuthorInfo(),
                   ],
                 );
               } else {
@@ -53,7 +60,7 @@ class SwiperDiy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300.w,
+      height: 300.h,
       width: 750.w,
       child: Swiper(
         itemBuilder: (BuildContext context, int index) {
@@ -71,7 +78,7 @@ class SwiperDiy extends StatelessWidget {
   }
 }
 
-//
+//菜单menu
 class TopNavigator extends StatelessWidget {
   final List<Post> navigatorList;
 
@@ -116,6 +123,60 @@ class TopNavigator extends StatelessWidget {
         children: navigatorList.map((item){
          return  _gridViewItemUI(context, item);
         }).toList(),
+      ),
+    );
+  }
+}
+
+//广告栏
+class AdBanner extends StatelessWidget {
+
+  final String adPicture;
+
+  AdBanner({Key key,this.adPicture}) :super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      child: Column(
+        children: <Widget>[
+          Image.network(adPicture,height: 100.h,width: 750.w,fit: BoxFit.fill,),
+          SizedBox(height: 4,),
+          Text("急，急，急，广告位招商..."),
+          SizedBox(height: 5,child: Container(color: Colors.grey[300],),),
+        ],
+      ),
+    );
+  }
+}
+
+//作者电话
+class AuthorInfo extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          CircleAvatar(
+            backgroundImage: NetworkImage("https://upload.jianshu.io/users/upload_avatars/2927423/37128c7f-32ea-45aa-8929-6fc64ef164e6.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240"),
+            radius: 60.w,
+          ),
+          SizedBox(width: 10),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("姓名：Chaoboyan"),
+              Text("简介：iOS从事多年，独立开发"),
+              Text("地址：https://github.com/ChaoBoyan"),
+              Text("邮箱：1297595138@qq.com"),
+            ],
+          ),
+        ],
       ),
     );
   }
